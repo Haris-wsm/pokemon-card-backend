8afe9d73413e82ae12910305c6127595ccdd435d
haris <hari.wm23@gmail.com>
2023-04-29 20:00:44 +0700

    Fix cors

--- app.js
+++ app.js
@@ -29,8 +29,11 @@ app.use(
 
 app.use(passport.initialize());
 
+// Set headers to allow credentials
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
